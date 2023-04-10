33ca419c8f1b4f9b9e28efa82852011d892e6eba
Chip <963255218@qq.com>
2023-04-10 16:28:02 +0800

    Default

---
+++ app/src/main/java/com/sunnyweather/android/app/SunnyWeatherApplication.kt
@@ -0,0 +1,27 @@
+package com.sunnyweather.android.app
+
+import android.annotation.SuppressLint
+import android.content.Context
+
+/**
+ *    author : Chip
+ *    time   : 2023/4/10
+ *    desc   :
+ */
+class SunnyWeatherApplication : AppApplication() {
+
+    companion object {
+        //彩云天气的令牌
+        const val TOKEN = "Uv6iF6RG6IsDjFKE"
+
+        //全局获取Context上下文
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+
+}
---
+++ app/src/main/java/com/sunnyweather/android/http/model/PlaceResponse.kt
@@ -0,0 +1,23 @@
+package com.sunnyweather.android.http.model
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ *    author : Chip
+ *    time   : 2023/4/10
+ *    desc   :
+ */
+
+data class PlaceResponse(
+    val places: List<Place>,
+    val query: String,
+    val status: String
+) {
+    data class Place(
+        @SerializedName("formatted_address") val address: String,
+        val location: Location,
+        val name: String,
+    ) {
+        data class Location(val lat: Double, val lng: Double)
+    }
+}
